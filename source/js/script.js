b38f9f9716b2d4afab7c72bad5dce96ba7c67672
slikitha <likhitha@navadhiti.com>
2023-10-06 11:50:06 +0530

    changes mentioned by client

--- source/js/script.js
+++ source/js/script.js
@@ -365,13 +365,13 @@ $(".subscribe-button").on("click", () => {
   subScribe();
 });
 
-const carousel = document.querySelector(".carouselfeatured");
-let cardIndex = 0;
+// const carousel = document.querySelector(".carouselfeatured");
+// let cardIndex = 0;
 
-setInterval(() => {
-  cardIndex = (cardIndex + 1) % 5;
-  carousel.style.transform = `translateX(-${cardIndex * 320}px)`; // Adjust the value to match your card width + margin
-}, 2000); // Adjust the duration for auto-sliding
+// setInterval(() => {
+//   cardIndex = (cardIndex + 1) % 5;
+//   carousel.style.transform = `translateX(-${cardIndex * 320}px)`; // Adjust the value to match your card width + margin
+// }, 2000); // Adjust the duration for auto-sliding
 // Get the modal
 const modal = document.getElementById("myModal");
 
